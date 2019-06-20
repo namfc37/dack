1c366e08d78720e45c441be8d1c86ed13d1a26a9
dhnam-fit <you@example.com>
2019-06-20 22:44:09 +0700

    updateall

---
+++ Source_Code/QuanLyThuVien/QuanLyThuVienDTO/CartBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.QuanLyThuVienDTO
+{
+    class CartBook
+    {
+        private static CartBook _ins;
+        public static CartBook Ins
+        {
+            get
+            {
+                if (_ins == null)
+                {
+                    _ins = new CartBook();
+                }
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+    }
+}
